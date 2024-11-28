97099223dd6a916f593f20e0859a7d384d4fdb7c
JuanNavarro <juan.navarro@veritas.cr>
2024-11-27 19:22:34 -0600

    xbox compatibility round 2

--- ProyectoUnity/Racoon Riot/Assets/Scripts/GameManager.cs
+++ ProyectoUnity/Racoon Riot/Assets/Scripts/GameManager.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
+    public EventSystem eventSystem;
     public GameObject menu;
+    public GameObject firstSelectedPause;
+
     int escCount = 0;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && escCount == 0)
+        if (eventSystem.currentSelectedGameObject == null)
         {
+            eventSystem.SetSelectedGameObject(firstSelectedPause);
+        }
+
+        if ((Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Pause")) && escCount == 0)
+        {
+            eventSystem.SetSelectedGameObject(firstSelectedPause);
             menu.SetActive(true);
             escCount = 1;
         }
-        else if (Input.GetKeyDown(KeyCode.Escape) && escCount == 1)
+        else if ((Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Pause")) && escCount == 1)
         {
             menu.SetActive(false);
             escCount = 0;
